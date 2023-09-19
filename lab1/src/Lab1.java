319a05efee65d3bb59bf0206a1cebae33b5d8f9e
Ilya <volkov.ilyya@gmail.com>
2023-09-19 10:13:42 +0200

    Initial commit

---
+++ lab1/src/Lab1.java
@@ -0,0 +1,31 @@
+public class Lab1 {
+    public static void main(String[] args) {
+        // C2 = 0; O1 = +; C = 1; O2 = +; int
+        int id = 1108;
+        int C = id % 3;
+        int n = 5;
+        int m = 5;
+        int a = 1;
+        int b = 1;
+
+        if (n <= a || m <= b) {
+            System.err.println("Invalid parameter values");
+            return;
+        }
+
+        double resultSum = 0.0;
+
+        for (int i = a; i <= n; i++) {
+            double innerSum = 0.0;
+
+            for (int j = b; j <= m; j++) {
+                double numerator = i + j;
+                double denominator = i + C;
+                innerSum += numerator / denominator;
+            }
+            resultSum += innerSum;
+        }
+
+        System.out.println("S = " + resultSum);
+    }
+}
